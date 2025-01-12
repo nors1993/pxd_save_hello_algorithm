3a3e15dc9abaef65e806b6a85ed13d4fedd038af
nors1993 <panxiandong1993@gmail.com>
2025-01-12 16:26:47 +0800

    add

--- 01_计算复杂度/00_time_complexity_pxd.py
+++ 01_计算复杂度/00_time_complexity_pxd.py
@@ -29,11 +29,38 @@ def quadratic(n:int) -> int:
             count += 1
     return count
 
+def bubble_sort(nums:list[int]) -> int:
+    '''平方阶（冒泡排序）'''
+    count = 0
+    # 外循环：未排序区间为[0, i]
+    for i in range(len(nums) - 1, 0, -1):
+        # 内循环：将未排序区间[0, i]中的最大元素交换至该区间的最右端
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                tmp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = tmp
+                count += 3 # 元素交换包含3个单元操作
+    return nums, count 
+
+def exponential(n: int) -> int:
+    '''指数阶（循环实现）'''
+    count = 1
+    base = 1
+    #print(base)
+    # 细胞每轮一分为二，形成数列 1, 2, 4, 8, ..., 2^(n-1)
+    for _ in range(n):
+        for _ in range(base):
+            count += 1
+            #
+        base *= 2
+        #print(base) 
+    return count 
 
 
 if __name__ == '__main__':
-    add = quadratic(8)
-    print(f"add:{add}")  # python 3.6以上版本支持
+    add = exponential(2) # 
+    print(f"🥷 :{add}")  # python 3.6以上版本支持
     # add_a = linear(50)
     # add_b = linear(100)
     #print(f"add_a:{add_a}, add_b:{add_b}")  # python 3.6以上版本支持
